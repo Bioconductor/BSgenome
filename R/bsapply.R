e1c14f04c29f398c6b54e1757794217bc4fb5832
Herve Pages <hpages@fhcrc.org>
2008-08-13 01:08:20 +0000

    #added some comments to Marc's early version of bsapply()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@33207 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/bsapply.R
+++ R/bsapply.R
@@ -1,7 +1,15 @@
 bsapply <- function(X, FUN, exclude = "rand", ...){
 
+    #HP: Please check the arguments. E.g. what would happen if the 'X'
+    #you get from the user was not a BSgenome object?
+
+    #HP: Please use <- instead of =
+
     ##get the csomes:
     csomes = seqnames(X)
+
+    #HP: The single line below is equivalent to the next 11 lines
+    #csomes <- csomes[-grep(exclude, csomes)]
 
     ##Restrict the csomes based on our exclusion factor:
     pariahIndex = grep(exclude, csomes)
@@ -15,6 +23,9 @@ bsapply <- function(X, FUN, exclude = "rand", ...){
     }    
     csomes = csomes[keepIndex]
     
+    #HP: You don't need this dirty trick. Just use X[[element]]
+    #in your processElement() function below.
+
     ##Some dirty tricks so that I can get the name of this thing:
     pkg = X@package
     pkgName = ls(paste("package:",pkg,sep =""))
@@ -22,14 +33,18 @@ bsapply <- function(X, FUN, exclude = "rand", ...){
     ##get a list of existing csome item the user has in memory 'right now'
     loadedCSomes = ls(X@.datacache_env)
     
+    #HP: Please use 'seqname' instead of 'element'
     ##Some stuff has to be done for each chromosome
     processElement = function(element, ...){
         str = paste(pkgName,"[[\"",element,"\"]]", sep ="")
         chr = eval(parse(text=str))
+        #HP: or just do chr <- X[[element]]
         result = FUN(chr, ...)
         ## Here is where I need to clean up after each of these
         ## just check if the thing is in the list, and if not = jettison
         if(element %in% loadedCSomes){
+            #HP: Or use cat()
+            #cat("Refusing to unload Chromosome:", element, "\n")
             print(paste("Refusing to unload Chromosome:", element))
         }else{
             print(paste("Unloading Chromosome:",element ))
@@ -37,6 +52,13 @@ bsapply <- function(X, FUN, exclude = "rand", ...){
         }
         return(result)
     }
+
+    #HP: Before you start the main loop, call on.exit(). See ?on.exit
+    #for the details. This guarantees that some code will be executed
+    #before the function actually exit, even in case of user interrupt
+    #or other kind of abnormal termination. This code must unload any
+    #sequence that is currently loaded but was not loaded when the function
+    #started.
 
     ##Then apply the above function to each
     list = lapply(csomes, processElement, ...)
