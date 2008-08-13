485fbdd4d1d46b9cae65f7ce69b2edd66791c80c
Marc Carlson <mcarlson@fhcrc.org>
2008-08-13 21:31:58 +0000

    Added corrections to bsapply()
    The biggest change is that this adds some code to try and cleanup any
    unwanted sequences that might be loaded during a call to bsapply() in
    the event that something bad happens.  Other changes are just tidying.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@33221 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/bsapply.R
+++ R/bsapply.R
@@ -3,66 +3,52 @@ bsapply <- function(X, FUN, exclude = "rand", ...){
     #HP: Please check the arguments. E.g. what would happen if the 'X'
     #you get from the user was not a BSgenome object?
 
-    #HP: Please use <- instead of =
-
     ##get the csomes:
-    csomes = seqnames(X)
-
-    #HP: The single line below is equivalent to the next 11 lines
-    #csomes <- csomes[-grep(exclude, csomes)]
+    csomes <- seqnames(X)
 
     ##Restrict the csomes based on our exclusion factor:
-    pariahIndex = grep(exclude, csomes)
-    keepIndex = logical()
-    for(i in 1:length(csomes)){
-        if(i %in% pariahIndex){
-            keepIndex = c(keepIndex, FALSE)
-        }else{
-            keepIndex = c(keepIndex, TRUE)
-        }
-    }    
-    csomes = csomes[keepIndex]
+    csomes <- csomes[-grep(exclude, csomes)]
     
-    #HP: You don't need this dirty trick. Just use X[[element]]
-    #in your processElement() function below.
-
-    ##Some dirty tricks so that I can get the name of this thing:
-    pkg = X@package
-    pkgName = ls(paste("package:",pkg,sep =""))
-
     ##get a list of existing csome item the user has in memory 'right now'
-    loadedCSomes = ls(X@.datacache_env)
+    loadedCSomes <- ls(X@.datacache_env)
     
-    #HP: Please use 'seqname' instead of 'element'
     ##Some stuff has to be done for each chromosome
-    processElement = function(element, ...){
-        str = paste(pkgName,"[[\"",element,"\"]]", sep ="")
-        chr = eval(parse(text=str))
-        #HP: or just do chr <- X[[element]]
-        result = FUN(chr, ...)
+    processSeqname <- function(seqname, ...){
+        result <- FUN(X[[seqname]], ...)
         ## Here is where I need to clean up after each of these
-        ## just check if the thing is in the list, and if not = jettison
-        if(element %in% loadedCSomes){
-            #HP: Or use cat()
-            #cat("Refusing to unload Chromosome:", element, "\n")
-            print(paste("Refusing to unload Chromosome:", element))
+        ## just check if the thing is in the list, and if not,
+        ## then jettison
+        if(seqname %in% loadedCSomes){
+            ##output will be dropped as we gain confidence,
+            ##or I can wrap it up so that it can be toggled off
+            cat("Keeping Chromosome:", seqname, "\n")
         }else{
-            print(paste("Unloading Chromosome:",element ))
-            unload(X, element)
+            cat("Unloading Chromosome:",seqname, "\n")
+            unload(X, seqname)
         }
         return(result)
     }
 
-    #HP: Before you start the main loop, call on.exit(). See ?on.exit
-    #for the details. This guarantees that some code will be executed
-    #before the function actually exit, even in case of user interrupt
-    #or other kind of abnormal termination. This code must unload any
-    #sequence that is currently loaded but was not loaded when the function
-    #started.
+    ##on.exit is called as an insurance policy. We just want to clear all
+    ##the memory "just in case" something got interrupted.
 
+    checkSeqs <- function(){
+            ##cat("Checking for any large loaded sequences", "\n")
+            for(i in seq_len(length(csomes))){
+                ##1st we check to make sure that it was NOT loaded at the beginning
+                if(!(csomes[i] %in% loadedCSomes)){
+                    ##Then we should also check if it is loaded "right now"
+                    if(csomes[i] %in% ls(X@.datacache_env)){
+                        cat("Unloading unwanted Chromosome:",csomes[i], "\n")
+                        unload(X, csomes[i])
+                    }
+                }}
+    }    
+    on.exit(checkSeqs())
+    
     ##Then apply the above function to each
-    list = lapply(csomes, processElement, ...)
-    names(list) = csomes
+    list <- lapply(csomes, processSeqname, ...)
+    names(list) <- csomes
     return(list)
 
 }
