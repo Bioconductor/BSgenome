524025a12bb0f049a49b27254e65b3b5547a7f55
Herve Pages <hpages@fhcrc.org>
2008-09-19 01:46:34 +0000

    added a template for BSgenome data packages to facilitate the process of forging them
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@33817 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/BSgenomeDataPkg-template/R/zzz.R
@@ -0,0 +1,14 @@
+@BSGENOMEOBJNAME@ <- BSgenome(
+    organism="@ORGANISM@",
+    species="@SPECIES@",
+    provider="@PROVIDER@",
+    provider_version="@PROVIDERVERSION@",
+    release_date="@RELEASEDATE@",
+    release_name="@RELEASENAME@",
+    source_url="@SOURCEURL@",
+    seqnames=@SEQNAMES@,
+    mseqnames=@MSEQNAMES@,
+    package="@PKGNAME@",
+    subdir="extdata"
+)
+
