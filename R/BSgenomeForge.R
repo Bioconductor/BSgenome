eca44ffb1cf4030b232d5686b7701d41194aba2d
Herve Pages <hpages@fhcrc.org>
2008-09-23 08:50:38 +0000

    #more cosmetic
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@33892 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/BSgenomeForge.R
+++ R/BSgenomeForge.R
@@ -389,8 +389,10 @@ setClass(
 {
     if (length(masks_objnames) == 0)
         return("")
-    paste(paste("data(", masks_objnames, ", package=\"", pkgname, "\")", sep=""),
-          collapse="\n")
+    usages <- paste(paste("data(", masks_objnames, ", package=\"", pkgname, "\")", sep=""),
+                    collapse="\n")
+    paste("## Load the masks of the single sequences contained in the package:",
+          usages, sep="\n")
 }
 
 setGeneric("forgeBSgenomeDataPkg", signature="x",
