f6cdf331373496d0978534ab9f3e36e06e3cd74b
Marc Carlson <mcarlson@fhcrc.org>
2008-09-30 22:25:55 +0000

    Adds a maskList toggle to bsapply.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@34088 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/bsapply.R
+++ R/bsapply.R
@@ -1,34 +1,78 @@
-bsapply <- function(X, FUN, exclude = "", simplify = FALSE, ...)
-{
+bsapply <- function(X, FUN, exclude = "", simplify = FALSE, maskList = c(), ...){
+
     ##Some argument checking.
     if(!is(X, "BSgenome")) stop("'X' must be a BSgenome object")
     if(!is.function(FUN)) stop("'FUN' must be a function")
     if(!is.character(exclude)) stop("'exclude' must be a character vector")
 
-    ##get the csomes:
-    csomes <- seqnames(X)
-    csomeLength <- length(csomes)
+    #Argument checking for maskList:
+    if(length(maskList)>0){ ##if there are masks
+        for(i in seq_len(length(maskList))){ #loop thru masks and deal with each in turn
+            if( !( maskList[i] %in% masknames(X)) ){stop("'maskList' must be vector of names corresponding to the default BSgenome masks.")}
+            ##     if( !( maskList[i] %in% masknames(X) || is.function(maskList[i]) || is(maskList[i], "IRanges") ) ){stop("'masks' must be an integer corresponding to the default BSgenome masks that are desired, an IRanges object that be used to generate a mask or a function that returns an IRanges object so that the masks can be generated.")}
+        }
+    }
 
-    ##Restrict the csomes based on our exclusion factor:
-    pariahIndex <- sapply(exclude, grep, csomes)
+            
+
+    
+    
+    ##get the seqnames:
+    seqnames <- seqnames(X)
+    seqLength = length(seqnames)
+
+    ##Restrict the seqnames based on our exclusion factor:
+    pariahIndex = numeric()
+    for(i in seq_len(length(exclude))){
+        ind = grep(exclude[i],seqnames)
+        ##Catenate the indices together as we go
+        pariahIndex = c(pariahIndex, ind)
+    }
 
     ##Added precaution in case some indices are found more than once...
     pariahIndex <- unique(pariahIndex)
-    ##IF the grepping has found something then we need to change our csomes
+    ##IF the grepping has found something then we need to change our seqnames
     ##But let's not be silly and allow people to exclude EVERYTHING.
-    if(length(pariahIndex) > 0 && length(pariahIndex) != csomeLength){
-        csomes <- csomes[-pariahIndex]
+    if(length(pariahIndex) > 0 && length(pariahIndex) != seqLength){
+        seqnames <- seqnames[-pariahIndex]
     }
     
-    ##Some stuff has to be done for each chromosome
-    processSeqname <- function(seqname, ...) FUN(X[[seqname]], ...)
 
+    ##Some stuff has to be done for each chromosome
+    processSeqname <- function(seqname, ...){
+
+        seq = X[[seqname]]
+        
+        if(length(maskList)>0){ ##if there are masks
+            for(i in seq_len(length(maskList))){ #loop thru masks and deal with each in turn
+                if(maskList[i] %in% masknames(X)){#IF its one of the names, then change its active state to be different from the default...
+                    if(active(masks(seq))[maskList[i]] == FALSE){
+                        active(masks(seq))[maskList[i]] <- TRUE
+                    }else{active(masks(seq))[maskList[i]] <- FALSE}
+                }
+##                 if(is.function(maskList[i])){#IF its a function, then lets call it to generate a GENOMIC RANGES object
+##                     ##
+##                 }
+            }
+        }
+
+
+        
+
+        ## This is where we finally get run the function that was passed in
+        result <- FUN(seq, ...)
+        return(result)
+    }
+
+    
     ##Then apply the above function to each
     if(simplify == FALSE){
-        ans <- lapply(csomes, processSeqname, ...)
-        names(ans) <- csomes
+        ans <- lapply(seqnames, processSeqname, ...)
+        names(ans) <- seqnames
+        return(ans)
     }else{
-        ans <- sapply(csomes, processSeqname, ...)
+        ans <- sapply(seqnames, processSeqname, ...)
+        return(ans)
     }
     ans
 }
