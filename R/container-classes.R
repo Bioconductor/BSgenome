cd04bd407d95277294aefc88c70a57f1ff89db85
Deepayan Sarkar <dsarkar@fhcrc.org>
2008-11-25 01:26:35 +0000

    tentative class for containing samle/chromosome level data
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@35471 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/container-classes.R
@@ -0,0 +1,47 @@
+
+
+
+## A container for data in the form of a list of chromosomes.  Each
+## sub-element can be anything
+
+setClass("ChromosomeData",
+         representation(genome="character"),
+         contains = "TypedList")
+
+## > showClass("ChromosomeData")
+## Class “ChromosomeData”
+
+## Slots:
+                                                                      
+## Name:           genome        elements           NAMES    elementClass
+## Class:       character            list characterORNULL       character
+
+## Extends: "TypedList"
+
+
+setValidity("ChromosomeData",
+            function(object) {
+                validElements <- lapply(elements(object), function(x) is(x, elementClass(object)))
+                if (!all(validElements))
+                    return(sprintf("Not all elements are of class '%s'",
+                                   elementClass(object)))
+            })
+            
+            
+setClass("SampleChromosomeData",
+         contains = "TypedList")
+
+
+setValidity("SampleChromosomeData",
+            function(object) {
+                if (!identical(elementClass(object), "ChromosomeData"))
+                    return("The elementClass(object) is not 'ChromosomeData'")
+                ## each element must be a "ChromosomeData"
+                validElements <- lapply(elements(object), function(x) is(x, elementClass(object)))
+                if (!all(validElements))
+                    return(sprintf("Not all elements are of class '%s'",
+                                   elementClass(object)))
+                TRUE
+            })
+
+
