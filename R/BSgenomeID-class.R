c24e4f9d95b079c4852741fba4cb5b7bc95fae7c
Michael Lawrence <lawremi@iastate.edu>
2008-12-28 16:28:15 +0000

    BSgenomeID class: formal representation of names of the form Celegans.UCSC.ce2.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@36319 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/BSgenomeID-class.R
@@ -0,0 +1,19 @@
+## Formal representation of a BSgenome ID (eg Celegans.UCSC.ce2)
+
+setClass("BSgenomeID", contains = "character",
+         validity = function(object) {
+           if (length(object) != 1)
+             "BSgenomeID must be a single string"
+           else if (length(.BSgenomeID.tokens(object)) != 3)
+             "BSgenomeID must have 3 tokens separated by '.'"
+           else NULL
+         })
+
+.BSgenomeID.tokens <- function(x) strsplit(x, "\\.")[[1]]
+
+setMethod("organism", "BSgenomeID", function(x) .BSgenomeID.tokens(x)[1])
+setMethod("provider", "BSgenomeID", function(x) .BSgenomeID.tokens(x)[2])
+setMethod("providerVersion", "BSgenomeID", function(x) .BSgenomeID.tokens(x)[3])
+
+setMethod("genome", "AnnotatedList",
+          function(x) new("BSgenomeID", annotation(x)))
