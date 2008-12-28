0f6fafbbce30f0749a5787b4bff5dd36a6613d42
Michael Lawrence <lawremi@iastate.edu>
2008-12-28 15:25:24 +0000

    Tweaks to GenomicRanges() constructor
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@36317 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/RangedData-methods.R
+++ R/RangedData-methods.R
@@ -109,11 +109,20 @@ setMethod("chrom", "RangesList", function(x) {
 ### Constructor
 ###
 
-GenomicRanges <- function(start, end, chrom = NULL, genome = NULL) {
+GenomicRanges <- function(start = integer(), end = integer(), chrom = NULL,
+                          genome = NULL)
+{
   ir <- IRanges(start, end)
-  if (!is.null(chrom))
+  if (!is.null(chrom)) {
+    if (!is.factor(chrom))
+      chrom <- factor(chrom, unique(chrom))
+    if (!length(ir))
+      chrom <- chrom[FALSE]
+    if (length(chrom) != length(ir))
+      stop("length of 'chrom' must equal that of 'start' and 'end'",
+           " unless 'start' and 'end' are zero length")
     rl <- split(ir, chrom)
-  else rl <- RangesList(ir)
+  } else rl <- RangesList(ir)
   universe(rl) <- genome
   rl
 }
