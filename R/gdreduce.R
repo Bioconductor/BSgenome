20e4ad79701ead15dd05e6b7c87ff52d545851a8
Martin Morgan <mtmorgan@fhcrc.org>
2009-06-30 19:47:04 +0000

    gdreduce, 'Reduce' sereral GenomeData or one GenomeDataList to GenomeData
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@40416 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/gdreduce.R
@@ -0,0 +1,45 @@
+setGeneric("gdreduce",
+           function(f, ..., init, right=FALSE, accumulate=FALSE,
+                    gdArgs=list()) standardGeneric("gdreduce"),
+           signature="...")
+
+setMethod(gdreduce, "GenomeDataList",
+          function(f, ..., init, right=FALSE, accumulate=FALSE,
+                   gdArgs=list())
+{
+    args <- list(...)
+    if (length(args) > 1L)
+        stop("'...' must satisfy 'length(...) == 1' in\n  ",
+             "'gdreduce,GenomeDataList-method'")
+    gdlist <- args[[1]]
+    gdnames <- lapply(gdlist, names)
+    nms <- unique(unlist(gdnames))
+    listData <-
+        if (missing(init))
+            lapply(nms, function(nm) {
+                ok <- sapply(gdnames, match, x=nm, nomatch=0L) > 0L
+                Reduce(f, lapply(gdlist[ok], "[[", nm), 
+                       right=right, accumulate=accumulate)
+            })
+        else
+            lapply(nms, function(nm) {
+                ok <- sapply(gdnames, match, x=nm, nomatch=0L) > 0L
+                Reduce(f, lapply(gdlist[ok], "[[", nm), init=init,
+                       right=right, accumulate=accumulate)
+            })
+    names(listData) <- nms
+    do.call(GenomeData, c(list(listData=listData), gdArgs))
+})
+
+setMethod(gdreduce, "GenomeData",
+          function(f, ..., init, right=FALSE, accumulate=FALSE,
+                   gdArgs=list())
+{
+    if (missing(init))
+        gdreduce(f, GenomeDataList(list(...)), right=right,
+                 accumulate=accumulate, gdArgs=gdArgs)
+    else
+        gdreduce(f, GenomeDataList(list(...)), init=init, right=right,
+                 accumulate=accumulate, gdArgs=gdArgs)
+
+})
