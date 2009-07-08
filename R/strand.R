90a35eec136d8cfed7f22663c0a9ade7f7340796
Patrick Aboyoun <unknown>
2009-07-08 18:37:51 +0000

    Further clean up of strand methods.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@40564 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/strand.R
+++ R/strand.R
@@ -6,17 +6,18 @@ setMethod("strand", "missing", function(x) factor(levels=c("-","+","*")))
 
 setMethod("strand", "character",
     function(x) {
-        lvls <- c(NA, levels(strand()))
-        if (!all(x %in% lvls | is.na(x)))
+        lvls <- levels(strand())
+        if (!all(is.na(x) | (x %in% lvls)))
             stop("strand values must be in '", paste(lvls, collapse="' '"), "'")
         factor(x, levels=lvls)
     })
 
 setMethod("strand", "DataTable",
     function(x) {
-        strand <- x[["strand"]]
-        if (is.null(strand))
-            strand <- rep(NA_character_, nrow(x))
-        levs <- levels(strand())
-        factor(strand, levs)
+        ans <- x[["strand"]]
+        if (is.null(ans))
+            ans <- rep(NA_character_, nrow(x))
+        if (is.character(ans))
+            ans <- strand(ans)
+        ans
     })
