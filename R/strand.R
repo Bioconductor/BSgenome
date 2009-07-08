f838f04f8912568a65112505d4394c37a6be6f47
Patrick Aboyoun <unknown>
2009-07-08 18:10:30 +0000

    Moved strand method for RangedData out of rtracklayer and into BSgenome as a method for DataTable.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@40562 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/strand.R
+++ R/strand.R
@@ -1,14 +1,22 @@
 ### The strand generic and the strand() method, for lack of a better home.
 
-setGeneric("strand", function(x, ...) standardGeneric("strand"))
+setGeneric("strand", function(x) standardGeneric("strand"))
 
-setMethod("strand", "missing", function(x) {
-  factor(levels=c("-","+","*"))
-})
+setMethod("strand", "missing", function(x) factor(levels=c("-","+","*")))
 
-setMethod(strand, "character", function(x, ...) {
-  lvls <- c(NA, levels(strand()))
-  if (!all(x %in% lvls | is.na(x)))
-    stop("strand values must be in '", paste(lvls, collapse="' '"), "'")
-  factor(x, levels=lvls)
-})
+setMethod("strand", "character",
+    function(x) {
+        lvls <- c(NA, levels(strand()))
+        if (!all(x %in% lvls | is.na(x)))
+            stop("strand values must be in '", paste(lvls, collapse="' '"), "'")
+        factor(x, levels=lvls)
+    })
+
+setMethod("strand", "DataTable",
+    function(x) {
+        strand <- x[["strand"]]
+        if (is.null(strand))
+            strand <- rep(NA_character_, nrow(x))
+        levs <- levels(strand())
+        factor(strand, levs)
+    })
