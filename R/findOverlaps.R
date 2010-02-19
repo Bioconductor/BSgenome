152fecf6b6feb86f074603c5bfb4e81db2b1946d
Seth Falcon <sfalcon@fhcrc.org>
2010-02-19 20:20:47 +0000

    More speedups to findOverlap,GRangesList,GRanges prototype
    Use direct slot access to create subset IRanges for pattern,
    factor invariants out of loops.
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@44771 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/findOverlaps.R
+++ R/findOverlaps.R
@@ -1,31 +1,35 @@
 findOverlaps1 <- function(pat, target)
 {
-    if (is(pat, "Alignments1")) {
-        pat <- as(pat, "GRangesList")
-    }
-    if (!is(target, "GRanges")) {
-        stop("'target' must be a GRanges object")
-    }
-    ans <- list()
-    uniqueSeqn <-
-      sort(intersect(unique(unlist(seqnames(pat), use.names=FALSE)),
-                     unique(seqnames(target))))
-    for (seqn in uniqueSeqn) {
-        for (strd in c("+", "-")) {
-            patWant <- seqnames(pat) == seqn & strand(pat) == strd
-            patRanges <- unlist(ranges(pat[patWant]), use.names=FALSE)
-            patIdx <-
-              seqselect(togroup(pat@partitioning),
-                        unlist(patWant, use.names=FALSE))
+    if (is(pat, "Alignments1")) pat <- as(pat, "GRangesList")
+    if (!is(target, "GRanges")) stop("'target' must be a GRanges object")
+    seqnames_target <- as.character(seqnames(target))
+    strand_target <- as.character(strand(target))
 
-            tarWant <- seqnames(target) == seqn & strand(target) == strd
-            tarIdx <- which(tarWant)
+    patgroup <- togroup(pat@partitioning)
+    ulData <- pat@unlistData
+    ourranges <- ulData@ranges
+    seqnames_pat <- as.character(ulData@seqnames)
+    strand_pat <- as.character(ulData@strand)
+
+    uniqueSeqn <- sort(intersect(unique(seqnames_pat), unique(seqnames_target)))
+    ans <- vector(mode = "list", length = length(uniqueSeqn) * 2L)
+    i <- 1L
+    for (seqn in uniqueSeqn) {
+        patWant0 <- seqnames_pat == seqn
+        tarWant0 <- seqnames_target == seqn
+        for (strd in c("+", "-")) {
+            patWant <- which(patWant0 & (strand_pat == strd))
+            patRanges <- ourranges[patWant]
+            patIdx <- patgroup[patWant]
+
+            tarIdx <- which(tarWant0 & (strand_target == strd))
             tarRanges <- ranges(target[tarIdx])
-            
+
             overlaps <- IRanges::findOverlaps(patRanges, tarRanges)
             queryIdx <- patIdx[queryHits(overlaps)]
             subjectIdx <- tarIdx[subjectHits(overlaps)]
-            ans <- c(ans, list(cbind(query = queryIdx, subject = subjectIdx)))
+            ans[[i]] <- cbind(query = queryIdx, subject = subjectIdx)
+            i <- i + 1L
         }
     }
     DIM <- c(length(target), length(pat))
