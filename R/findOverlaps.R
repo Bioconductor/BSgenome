0863ff2dac62177beed64906031625f02151d959
Patrick Aboyoun <unknown>
2010-02-19 17:19:22 +0000

    Speedup to findOverlap,GRangesList,GRanges prototype.
    Removed an inner lapply loop and used more advanced
    Sequence infrastructure.
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@44768 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/findOverlaps.R
+++ R/findOverlaps.R
@@ -3,24 +3,34 @@ findOverlaps1 <- function(pat, target)
     if (is(pat, "Alignments1")) {
         pat <- as(pat, "GRangesList")
     }
+    if (!is(target, "GRanges")) {
+        stop("'target' must be a GRanges object")
+    }
     ans <- list()
-    for (seqn in sort(unique(unlist(seqnames(pat))))) {
+    uniqueSeqn <-
+      sort(intersect(unique(unlist(seqnames(pat), use.names=FALSE)),
+                     unique(seqnames(target))))
+    for (seqn in uniqueSeqn) {
         for (strd in c("+", "-")) {
-            patranges <- ranges(pat[seqnames(pat) == seqn & strand(pat) == strd])
-            tarwant <- seqnames(target) == seqn & strand(target) == strd
-            taridx <- which(tarwant)
-            tarranges <- ranges(target[taridx])
-            lapply(seq_len(length(patranges)), function(i)
-               {
-                   x <- IRanges::findOverlaps(patranges[[i]], tarranges)
-                   subjectIdx <- taridx[subjectHits(x)]
-                   ans <<- c(ans, list(cbind(query=rep(i, length(subjectIdx)),
-                                             subject=subjectIdx)))
-               })
+            patWant <- seqnames(pat) == seqn & strand(pat) == strd
+            patRanges <- unlist(ranges(pat[patWant]), use.names=FALSE)
+            patIdx <-
+              seqselect(togroup(pat@partitioning),
+                        unlist(patWant, use.names=FALSE))
+
+            tarWant <- seqnames(target) == seqn & strand(target) == strd
+            tarIdx <- which(tarWant)
+            tarRanges <- ranges(target[tarIdx])
+            
+            overlaps <- IRanges::findOverlaps(patRanges, tarRanges)
+            queryIdx <- patIdx[queryHits(overlaps)]
+            subjectIdx <- tarIdx[subjectHits(overlaps)]
+            ans <- c(ans, list(cbind(query = queryIdx, subject = subjectIdx)))
         }
     }
     DIM <- c(length(target), length(pat))
     m <- do.call(rbind, ans)
-    new("RangesMatching", matchMatrix=m[order(m[ , 1L], m[ , 2L]), ],
+    new("RangesMatching",
+        matchMatrix = m[order(m[ , 1L], m[ , 2L]), , drop=FALSE],
         DIM = DIM)
 }
