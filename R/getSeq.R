e3f3ffc8f59b8bfa21a5ada0732d879b0e043566
Herve Pages <hpages@fhcrc.org>
2010-06-12 00:27:01 +0000

    Polishing getSeq() new code and man page.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@47415 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getSeq.R
+++ R/getSeq.R
@@ -2,6 +2,11 @@
 ### getSeq()
 ### -------------------------------------------------------------------------
 
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Low-level helpers called by "getSeq" method for BSgenome objects.
+###
+
 .normargStrand <- function(strand, length)
 {
     if (is(strand, "Rle"))
@@ -38,16 +43,6 @@
     width <- IRanges:::recycleVector(width, max0123)
     strand <- .normargStrand(strand, max0123)
     list(names=names, start=start, end=end, width=width, strand=strand)
-}
-
-### Assumes 'seqlengths' is a named integer vector and 'names' is a character
-### vector with values in 'names(seqlengths)'.
-.newGRangesFromGetSeqArgs <- function(seqlengths, names,
-                                      start, end, width, strand)
-{
-    refwidths <- seqlengths[names]
-    ranges <- solveUserSEW(refwidths, start=start, end=end, width=width)
-    GRanges(seqnames=names, ranges=ranges, strand=strand)
 }
 
 ### Assumes 'x' is a RangesList object with names.
@@ -108,6 +103,11 @@
             seqlengths=seqlengths(x)[unique(seqnames)])
 }
 
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### .extractFromBSgenomeSingleSequences()
+###
+
 ### Assumes 'ranges' and 'strand' have the same length and that this
 ### length is >= 1.
 .extractSeqsFromDNAString <- function(subject, ranges, strand)
@@ -138,19 +138,29 @@
                                as.factor(strand))
 }
 
-.extractSeqsFromBSgenome <- function(x, granges)
+### 'names' must be a character vector or a GRanges object.
+### If 'names' is character vector, then 'start', 'end', 'width', and 'strand'
+### are assumed to be already normalized. Otherwise, they are ignored.
+.extractFromBSgenomeSingleSequences <- function(x, names,
+                                                start, end, width, strand)
 {
-    ## Check that 'seqlengths(granges)' is compatible with 'x'.
-    #if (!all(names(seqlengths(granges)) %in% names(seqlengths(x))))
+    if (is.character(names)) {
+        refwidths <- seqlengths(x)[names]
+        ranges <- solveUserSEW(refwidths, start=start, end=end, width=width)
+        names <- GRanges(seqnames=names, ranges=ranges, strand=strand)
+    }
+
+    ## Check that 'seqlengths(names)' is compatible with 'x'.
+    #if (!all(names(seqlengths(names)) %in% names(seqlengths(x))))
     #    stop("sequence names in GRanges are incompatible with BSgenome object")
-    #if (!all(is.na(seqlengths(granges)))
-    # && !identical(seqlengths(granges),
-    #               seqlengths(x)[names(seqlengths(granges))]))
+    #if (!all(is.na(seqlengths(names)))
+    # && !identical(seqlengths(names),
+    #               seqlengths(x)[names(seqlengths(names))]))
     #    stop("sequence lengths in GRanges are incompatible ",
     #         "with BSgenome object")
 
-    ## Split 'granges' by sequence names.
-    grglist <- split(granges, seqnames(granges))
+    ## Split 'names' by sequence names.
+    grglist <- split(names, seqnames(names))
     ## Loop over the sequence names and extract the ranges.
     dnaset_list <- lapply(seq_len(length(grglist)),
         function(i)
@@ -165,10 +175,15 @@
     )
     ## "unsplit" 'dnaset_list'.
     unsplit.list.of.XStringSet("DNAStringSet", dnaset_list,
-                               as.factor(seqnames(granges)))
+                               as.factor(seqnames(names)))
 }
 
-.extractSpecialSeq <- function(x, name, start, end, width, strand)
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### .extractFromBSgenomeMultipleSequences()
+###
+
+.getOneSeqFromBSgenomeMultipleSequences <- function(x, name,
+                                                    start, end, width, strand)
 {
     nhits <- 0L
     for (mseqname in mseqnames(x)) {
@@ -203,31 +218,38 @@
     DNAStringSet(successiveViews(subject, elementLengths(x)))
 }
 
-.extractSpecialSeqsFromBSgenome <- function(x, granges)
+### 'names' must be a character vector or a GRanges object.
+### If 'names' is character vector, then 'start', 'end', 'width', and 'strand'
+### are assumed to be already normalized. Otherwise, they are ignored.
+.extractFromBSgenomeMultipleSequences <- function(x, names,
+                                                  start, end, width, strand)
 {
-    ans <- lapply(seq_len(length(granges)),
-                  function(i)
-                  {
-                      name <- as.character(seqnames(granges))[i]
-                      start <- start(granges)[i]
-                      width <- width(granges)[i]
-                      strand <- as.character(strand(granges))[i]
-                      .extractSpecialSeq(x, name, start, NA, width, strand)
-                  }
-           )
+    if (is.character(names)) {
+        ans <- lapply(seq_len(length(names)),
+            function(i)
+                .getOneSeqFromBSgenomeMultipleSequences(x, names[i],
+                                start[i], end[i], width[i], strand[i])
+        )
+    } else {
+        ans <- lapply(seq_len(length(names)),
+            function(i)
+            {
+                name <- as.character(seqnames(names))[i]
+                start <- start(names)[i]
+                width <- width(names)[i]
+                strand <- as.character(strand(names))[i]
+                .getOneSeqFromBSgenomeMultipleSequences(x, name,
+                                start, NA, width, strand)
+            }
+        )
+    }
     .listOfDNAStringToDNAStringSet(ans)
 }
 
-.extractSpecialSeqsFromBSgenome2 <- function(x, names,
-                                             start, end, width, strand)
-{
-    ans <- lapply(seq_len(length(names)),
-               function(i)
-                   .extractSpecialSeq(x, names[i],
-                                      start[i], end[i], width[i], strand[i])
-           )
-    .listOfDNAStringToDNAStringSet(ans)
-}
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### The "getSeq" generic and method for BSgenome objects.
+###
 
 setGeneric("getSeq", function(x, ...) standardGeneric("getSeq"))
 
@@ -247,46 +269,48 @@ setMethod("getSeq", "BSgenome",
         }
         if (is.character(names)) {
             args <- .normGetSeqArgs(names, start, end, width, strand)
-            is_not_special <- args$names %in% seqnames(x)
-            is_special <- !is_not_special
-            ans <- rep.int(DNAStringSet(""), length(args$names))
-            ans[is_special] <- .extractSpecialSeqsFromBSgenome2(x,
-                                   args$names[is_special],
-                                   args$start[is_special],
-                                   args$end[is_special],
-                                   args$width[is_special],
-                                   args$strand[is_special])
-            granges <- .newGRangesFromGetSeqArgs(seqlengths(x),
-                           args$names[is_not_special],
-                           args$start[is_not_special],
-                           args$end[is_not_special],
-                           args$width[is_not_special],
-                           args$strand[is_not_special])
-            ans[is_not_special] <- .extractSeqsFromBSgenome(x, granges)
-            if (length(ans) == 1L)
-                ans <- ans[[1L]]  # turn 1st and unique element into DNAString
+            sseq_idx <- args$names %in% seqnames(x)
+            sseq_args <- list(names=args$names[sseq_idx],
+                              start=args$start[sseq_idx],
+                              end=args$end[sseq_idx],
+                              width=args$width[sseq_idx],
+                              strand=args$strand[sseq_idx])
+            mseq_idx <- !sseq_idx
+            mseq_args <- list(names=args$names[mseq_idx],
+                              start=args$start[mseq_idx],
+                              end=args$end[mseq_idx],
+                              width=args$width[mseq_idx],
+                              strand=args$strand[mseq_idx])
         } else {
             if (!identical(c(start, end, width), c(NA, NA, NA)))
                 stop("'start', 'end' and 'width' can only be specified when ",
                      "'names' is either missing, a character vector/factor, ",
                      "a character-Rle, or a factor-Rle")
-            granges <- .newGRanges(names, strand)
-            seqnames <- as.character(seqnames(granges))
-            is_not_special <- seqnames %in% seqnames(x)
-            is_special <- !is_not_special
-            if (any(is_special)) {
-                ans <- rep.int(DNAStringSet(""), length(is_special))
-                ans[is_special] <-
-                    .extractSpecialSeqsFromBSgenome(x, granges[is_special])
-                granges <- .dropUnusedGRangesSeqnamesLevels(
-                               granges[is_not_special])
-                ans[is_not_special] <- .extractSeqsFromBSgenome(x, granges)
-            } else {
-                ans <- .extractSeqsFromBSgenome(x, granges)
-            }
+            names <- .newGRanges(names, strand)
+            seqnames <- as.character(seqnames(names))
+            sseq_idx <- seqnames %in% seqnames(x)
+            sseq_args <- list(names=.dropUnusedGRangesSeqnamesLevels(
+                                    names[sseq_idx]))
+            mseq_idx <- !sseq_idx
+            mseq_args <- list(names=names[mseq_idx])
         }
+        ans <- rep.int(DNAStringSet(""), length(sseq_idx))
+        ans[sseq_idx] <- .extractFromBSgenomeSingleSequences(x,
+                                   sseq_args$names,
+                                   sseq_args$start,
+                                   sseq_args$end,
+                                   sseq_args$width,
+                                   sseq_args$strand)
+        ans[mseq_idx] <- .extractFromBSgenomeMultipleSequences(x,
+                                   mseq_args$names,
+                                   mseq_args$start,
+                                   mseq_args$end,
+                                   mseq_args$width,
+                                   mseq_args$strand)
         if (as.character)
             ans <- as.character(ans)
+        else if (length(ans) == 1L && is.character(names))
+            ans <- ans[[1L]]  # turn 1st and unique element into DNAString
         ans
     }
 )
