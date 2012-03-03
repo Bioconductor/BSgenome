87a80ca85a8a8e783bdae01c14f35e994821c15c
Herve Pages <hpages@fhcrc.org>
2012-03-03 19:38:42 +0000

    using factors only where appropriate
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@63415 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/available.genomes.R
+++ R/available.genomes.R
@@ -1,27 +1,30 @@
 .splitNameParts <- function(pkgs)
 {
-    tmp <- strsplit(pkgs, ".", fixed=TRUE)
-    eltlens <- elementLengths(tmp)
+    parts <- strsplit(pkgs, ".", fixed=TRUE)
+    nparts <- elementLengths(parts)
     ## Some packages like BSgenome.Tgondii.ToxoDB.7.0 don't follow the rules
     ## and are made of more than 4 parts.
-    has_more_than_4_parts <- which(eltlens > 4L)
+    has_more_than_4_parts <- which(nparts > 4L)
     for (i in has_more_than_4_parts) {
-        tmp_i <- tmp[[i]]
-        tmp[[i]] <- c(tmp_i[1:3], paste(tmp_i[4:eltlens[i]], collapse="."))
+        tmp <- parts[[i]]
+        parts[[i]] <- c(tmp[1:3], paste(tmp[4:length(tmp)], collapse="."))
     }
     ## And just in case one day we start to see some packages with less than
     ## 4 parts.
-    has_less_than_4_parts <- which(eltlens < 4L)
-    for (i in has_less_than_4_parts)
-        tmp[[i]] <- c(tmp[[i]], rep.int(NA_character_, 4L - eltlens[i]))
-    ## From now, any top-level element in 'tmp' is guaranteed to be a character
-    ## vector of length 4.
-    utmp <- unlist(tmp)
-    idx4 <- (1:length(tmp)) * 4L
+    has_less_than_4_parts <- which(nparts < 4L)
+    for (i in has_less_than_4_parts) {
+        tmp <- parts[[i]]
+        parts[[i]] <- c(tmp, rep.int(NA_character_, 4L - length(tmp)))
+    }
+    ## From now, any top-level element in 'parts' is guaranteed to be a
+    ## character vector of length 4.
+    uparts <- unlist(parts)
+    idx4 <- (1:length(parts)) * 4L
     data.frame(pkgname=pkgs,
-               organism=utmp[idx4 - 2L],
-               provider=utmp[idx4 - 1L],
-               provider_version=utmp[idx4])
+               organism=factor(uparts[idx4 - 2L]),
+               provider=factor(uparts[idx4 - 1L]),
+               provider_version=uparts[idx4],
+               stringsAsFactors=FALSE)
 }
 
 installed.genomes <- function(splitNameParts=FALSE)
