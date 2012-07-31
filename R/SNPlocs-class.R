c45fcd8a1df2c9cec46fb0f636a4a66504ff552d
Herve Pages <hpages@fhcrc.org>
2012-07-31 20:22:15 +0000

    add 'provider_version' slot to SNPlocs class
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@68116 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/SNPlocs-class.R
+++ R/SNPlocs-class.R
@@ -7,10 +7,16 @@ setClass("SNPlocs",
         ## provider of the SNPs (e.g. "dbSNP")
         provider="character",
 
+        ## creation date (in compact format) of the flat files found at
+        ## download_url (look at 2nd line of each file e.g.
+        ## CREATED ON: 2012-06-08 14:53, and use the most recent date in
+        ## case of mixed dates, e.g. "20120608")
+        provider_version="character",
+
         ## official release date of the SNPs (e.g. "Nov 9, 2010")
         release_date="character",
 
-        ## provider version of the SNPs (e.g. "Build 132")
+        ## official release name of the SNPs (e.g. "Build 132")
         release_name="character",
 
         ## URL to the place where the original SNP data was downloaded
@@ -42,13 +48,15 @@ setClass("SNPlocs",
 ###
 
 ### Not intended to be used directly.
-newSNPlocs <- function(provider, release_date, release_name,
+newSNPlocs <- function(provider, provider_version,
+                       release_date, release_name,
                        download_url, download_date,
                        reference_genome, compatible_genomes,
                        data_pkgname, data_dirpath)
 {
     new("SNPlocs",
         provider=provider,
+        provider_version=provider_version,
         release_date=release_date,
         release_name=release_name,
         download_url=download_url,
