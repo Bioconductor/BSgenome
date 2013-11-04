c56f42a922487a8c7c907075ab4bc113f39c9098
Herve Pages <hpages@fhcrc.org>
2013-11-04 23:56:52 +0000

    Rename FastaBSgenome class -> FaRzBSgenome.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@82913 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/FaRzBSgenome-class.R
+++ R/FaRzBSgenome-class.R
@@ -1,22 +1,23 @@
 ### =========================================================================
-### FastaBSgenome objects
+### FaRzBSgenome objects
 ### -------------------------------------------------------------------------
 
 
-setClass("FastaBSgenome",
+setClass("FaRzBSgenome",
     contains="BSgenome",
     representation(
-        ## FaFile object containing the sequences
+        ## FaFile object pointing to the .fa.rz file (and .fa.rz.fai index)
+        ## containing the sequences.
         fafile="FaFile"
     )
 )
 
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### FastaBSgenome constructor (NOT exported)
+### FaRzBSgenome constructor (NOT exported)
 ###
 
-.makeFastaBSgenomeSeqinfo <- function(fafile, circ_seqs, provider_version)
+.makeFaRzBSgenomeSeqinfo <- function(fafile, circ_seqs, provider_version)
 {
     seqlengths <- seqlengths(fafile)
     seqnames <- names(seqlengths)
@@ -31,22 +32,22 @@ setClass("FastaBSgenome",
             genome=provider_version)
 }
 
-FastaBSgenome <- function(filepath, circ_seqs=NA,
-                          nmask_per_seq=0, masks_dirpath,
-                          organism, species, provider, provider_version,
-                          release_date, release_name, source_url,
-                          pkgname=NA)
+FaRzBSgenome <- function(filepath, circ_seqs=NA,
+                         nmask_per_seq=0, masks_dirpath,
+                         organism, species, provider, provider_version,
+                         release_date, release_name, source_url,
+                         pkgname=NA)
 {
     fafile <- FaFile(filepath)
     open(fafile)
-    seqinfo <- .makeFastaBSgenomeSeqinfo(fafile, circ_seqs, provider_version)
+    seqinfo <- .makeFaRzBSgenomeSeqinfo(fafile, circ_seqs, provider_version)
     user_seqnames <- seqnames(seqinfo)
     names(user_seqnames) <- user_seqnames
     genome_description <- GenomeDescription(organism, species,
                                             provider, provider_version,
                                             release_date, release_name,
                                             seqinfo)
-    new("FastaBSgenome", genome_description,
+    new("FaRzBSgenome", genome_description,
         source_url=source_url,
         user_seqnames=user_seqnames,
         mseqnames=character(0),
@@ -63,7 +64,7 @@ FastaBSgenome <- function(filepath, circ_seqs=NA,
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ### BSgenome constructor
 ###
-### Returns either an RdaBSgenome or a FastaBSgenome object.
+### Returns either an RdaBSgenome or a FaRzBSgenome object.
 ###
 
 BSgenome <- function(organism, species, provider, provider_version,
@@ -72,14 +73,14 @@ BSgenome <- function(organism, species, provider, provider_version,
                      seqs_pkgname, seqs_dirpath,
                      nmask_per_seq, masks_pkgname, masks_dirpath)
 {
-    fasta_filename <- paste0(provider_version, ".fa.rz")
-    fasta_filepath <- file.path(seqs_dirpath, fasta_filename)
-    if (file.exists(fasta_filepath)) {
-        FastaBSgenome(fasta_filepath, circ_seqs,
-                      nmask_per_seq, masks_dirpath,
-                      organism, species, provider, provider_version,
-                      release_date, release_name, source_url,
-                      masks_pkgname)
+    farz_filename <- paste0(provider_version, ".fa.rz")
+    farz_filepath <- file.path(seqs_dirpath, farz_filename)
+    if (file.exists(farz_filepath)) {
+        FaRzBSgenome(farz_filepath, circ_seqs,
+                     nmask_per_seq, masks_dirpath,
+                     organism, species, provider, provider_version,
+                     release_date, release_name, source_url,
+                     masks_pkgname)
     } else {
         RdaBSgenome(organism, species, provider, provider_version,
                     release_date, release_name, source_url,
@@ -100,7 +101,7 @@ BSgenome <- function(organism, species, provider, provider_version,
     scanFa(x@fafile, param=param)[[1L]]
 }
 
-setMethod("loadBSgenomeNakedSequence", "FastaBSgenome",
+setMethod("loadBSgenomeNakedSequence", "FaRzBSgenome",
     function(x, seqname) .loadFastaSequence(x, seqname)
 )
 
