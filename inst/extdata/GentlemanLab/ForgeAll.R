cb53eabcaa1fc35cab805e6dece691a19242ec90
Herve Pages <hpages@fhcrc.org>
2014-02-10 21:08:52 +0000

    Add seed file for susScr3 masked genome.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@86285 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/extdata/GentlemanLab/ForgeAll.R
+++ inst/extdata/GentlemanLab/ForgeAll.R
@@ -84,7 +84,8 @@ MaskedBSgenome_datapkgs <- c(
   "BSgenome.Ptroglodytes.UCSC.panTro2.masked",
   "BSgenome.Ptroglodytes.UCSC.panTro3.masked",
   "BSgenome.Rnorvegicus.UCSC.rn4.masked",
-  "BSgenome.Rnorvegicus.UCSC.rn5.masked"
+  "BSgenome.Rnorvegicus.UCSC.rn5.masked",
+  "BSgenome.Sscrofa.UCSC.susScr3.masked"
 )
 
 for (pkg in MaskedBSgenome_datapkgs) {
