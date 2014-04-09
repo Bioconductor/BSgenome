8226e246b49afaf073d897a540059b4072a503db
Herve Pages <hpages@fhcrc.org>
2014-04-09 06:07:28 +0000

    Use non-exported utility from GenomeInfoDb to fetch the mapping from GenBank accessions to GRCh38 seqlevels.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@88646 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/extdata/GentlemanLab/BSgenome.Hsapiens.NCBI.GRCh38-tools/splitbigfasta.R
+++ inst/extdata/GentlemanLab/BSgenome.Hsapiens.NCBI.GRCh38-tools/splitbigfasta.R
@@ -1,5 +1,6 @@
 ###
 library(Biostrings)
+library(GenomeInfoDb)
 
 ### Download GCA_000001405.15_GRCh38_top-level.fna.gz from
 ### ftp://ftp.ncbi.nlm.nih.gov/genbank/genomes/Eukaryotes/vertebrates_mammals/Homo_sapiens/GRCh38
@@ -7,19 +8,16 @@ library(Biostrings)
 GRCh38 <- readDNAStringSet("GCA_000001405.15_GRCh38_top-level.fna")
 m_data <- strsplit(names(GRCh38), "|", fixed=TRUE)
 fasta_headers <- matrix(unlist(m_data, use.names=FALSE), ncol=length(m_data))
-accn <- fasta_headers[4L, ]
-stopifnot(!any(duplicated(accn)))
+GRCh38_accns <- fasta_headers[4L, ]
+stopifnot(!any(duplicated(GRCh38_accns)))
 
-assembly_report <- read.table("ftp://ftp.ncbi.nlm.nih.gov/genbank/genomes/Eukaryotes/vertebrates_mammals/Homo_sapiens/GRCh38/GCA_000001405.15_GRCh38_assembly_report.txt", sep="\t", stringsAsFactors=FALSE)
-accn2 <- assembly_report[[5L]]
-stopifnot(!any(duplicated(accn2)))
+GRCh38_accn2seqlevel <- GenomeInfoDb:::fetch_GenBankAccn2seqlevel_for_GRCh38()
 
-stopifnot(setequal(accn, accn2))
+stopifnot(setequal(GRCh38_accns, names(GRCh38_accn2seqlevel)))
+GRCh38_seqlevels <- GRCh38_accn2seqlevel[GRCh38_accns]
 
-seqnames <- assembly_report[[1L]][match(accn, accn2)]
-
-for (i in seq_along(accn)) {
-    filename <- paste0(seqnames[[i]], ".fa")
+for (i in seq_along(GRCh38)) {
+    filename <- paste0(GRCh38_seqlevels[[i]], ".fa")
     cat("writing ", filename, "\n", sep="")
     writeXStringSet(GRCh38[i], file=filename, width=50L)
 }
