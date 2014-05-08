64c416d7ccaf2780c127376618439b62cbb6786b
Herve Pages <hpages@fhcrc.org>
2014-05-08 20:03:36 +0000

    import rtracklayer and move "export" methods for BSgenome objects from it
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@90099 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/export-methods.R
@@ -0,0 +1,48 @@
+### =========================================================================
+### Some "export" methods for BSgenome objects
+### -------------------------------------------------------------------------
+###
+### Note: the export() generic is defined in the rtracklayer package.
+###
+
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Export as FASTA file.
+###
+
+setMethod("export", c("BSgenome", "FastaFile"),
+    function(object, con, format, ...)
+    {
+        append <- FALSE
+        for (seqname in seqnames(object)) {
+            dna <- object[[seqname]]
+            masks(dna) <- NULL
+            dna <- DNAStringSet(dna)
+            names(dna) <- seqname
+            writeXStringSet(dna, path(con), append = append, ...)
+            append <- TRUE
+        }
+    }
+)
+
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Export as twoBit file.
+###
+
+setMethod("export", c("BSgenome", "TwoBitFile"),
+    function(object, con, format, ...)
+    {
+        if (!missing(format))
+            checkArgFormat(con, format)
+        i <- 0L
+        twoBits <- bsapply(
+            new("BSParams", X=object, FUN=function(chr) {
+                i <<- i + 1
+                rtracklayer:::.DNAString_to_twoBit(chr, seqnames(object)[i])
+            }, ...))
+        invisible(rtracklayer:::.TwoBits_export(as.list(twoBits),
+                                                twoBitPath(path(con))))
+    }
+)
+
