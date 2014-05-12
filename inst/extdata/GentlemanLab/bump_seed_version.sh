581fc57c2e8f011ce9a4f9edff9918c3be835d89
Herve Pages <hpages@fhcrc.org>
2014-05-12 23:29:13 +0000

    bump versions in seed files before reforging BSgenome data packages
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@90217 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/extdata/GentlemanLab/bump_seed_version.sh
+++ inst/extdata/GentlemanLab/bump_seed_version.sh
@@ -2,9 +2,10 @@
 
 set -e  # Exit immediately if a simple command exits with a non-zero status
 
-for seed in BSgenome.*-seed; do
+#for seed in BSgenome.*-seed; do
+for seed in `ls BSgenome.*-seed | grep -v '\.masked-seed'`; do
 	mv $seed ${seed}.old
-	cat ${seed}.old | sed -r "s/^Version: 1\.3\.19$/Version: 1.3.99/" > $seed
+	cat ${seed}.old | sed -r "s/^Version: 1\.3\.99$/Version: 1.4.0/" > $seed
 	rm ${seed}.old
 done
 
