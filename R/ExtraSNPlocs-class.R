08639b21c26ff77261db3e715db0435a98f86d6b
Herve Pages <hpages@fhcrc.org>
2014-10-22 01:56:59 +0000

    use an OnDiskLongTable slot in ExtraSNPlocs objects to store the SNP table
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@95845 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ExtraSNPlocs-class.R
+++ R/ExtraSNPlocs-class.R
@@ -1,40 +1,37 @@
 ### =========================================================================
 ### ExtraSNPlocs objects
 ### -------------------------------------------------------------------------
+###
 
 setClass("ExtraSNPlocs",
     representation(
-        ## provider of the SNPs (e.g. "dbSNP")
+        ## Name of the ExtraSNPlocs data package where the ExtraSNPlocs
+        ## object is defined.
+        pkgname="character",
+
+        ## OnDiskLongTable object containing the SNP table.
+        snp_table="OnDiskLongTable",
+
+        ## Provider of the SNPs (e.g. "dbSNP").
         provider="character",
 
-        ## creation date (in compact format) of the flat files found at
-        ## download_url (look at 2nd line of each file e.g.
-        ## CREATED ON: 2012-06-08 14:53, and use the most recent date in
-        ## case of mixed dates, e.g. "20120608")
+        ## E.g. "dbSNP Human BUILD 141".
         provider_version="character",
 
-        ## official release date of the SNPs (e.g. "Nov 9, 2010")
+        ## Official release date of the SNPs (e.g. "May 2014").
         release_date="character",
 
-        ## official release name of the SNPs (e.g. "Build 132")
+        ## Official release name of the SNPs (e.g. "dbSNP Human BUILD 141").
         release_name="character",
 
-        ## URL to the place where the original SNP data was downloaded
+        ## URL to the place where the original SNP data was downloaded.
         download_url="character",
 
-        ## date the original SNP data was downloaded
+        ## Date the original SNP data was downloaded.
         download_date="character",
 
-        ## reference genome of the SNPs
-        reference_genome="GenomeDescription",
-
-        ## package name and absolute path to local directory where to find
-        ## the serialized objects containing the SNPs
-        data_pkgname="character",
-        data_dirpath="character",
-        data_serialized_objnames="character",
-
-        .data_cache="environment"
+        ## Reference genome of the SNPs.
+        reference_genome="GenomeDescription"
     )
 )
 
@@ -67,28 +64,23 @@ setMethod("seqnames", "ExtraSNPlocs", function(x) seqnames(referenceGenome(x)))
 ###
 
 ### Not intended to be used directly.
-newExtraSNPlocs <- function(provider, provider_version,
+newExtraSNPlocs <- function(pkgname, snp_table_dirpath,
+                            provider, provider_version,
                             release_date, release_name,
                             download_url, download_date,
-                            reference_genome,
-                            data_pkgname, data_dirpath)
+                            reference_genome)
 {
-    data_serialized_objnames <- c(
-        "ExtraSNPcount",
-        paste(seqlevels(reference_genome), "_extrasnplocs", sep="")
-    )
+    snp_table <- OnDiskLongTable(snp_table_dirpath)
     new("ExtraSNPlocs",
+        pkgname=pkgname,
+        snp_table=snp_table,
         provider=provider,
         provider_version=provider_version,
         release_date=release_date,
         release_name=release_name,
         download_url=download_url,
         download_date=download_date,
-        reference_genome=reference_genome,
-        data_pkgname=data_pkgname,
-        data_dirpath=data_dirpath,
-        data_serialized_objnames=data_serialized_objnames,
-        .data_cache=new.env(hash=TRUE, parent=emptyenv()))
+        reference_genome=reference_genome)
 }
 
 
@@ -109,170 +101,19 @@ setMethod("show", "ExtraSNPlocs",
 ### snpcount() and snplocs()
 ###
 
-### Creates objects "on-the-fly" (not serialized).
-### WARNING: Improper calls to .get_ExtraSNPlocs_data() by the
-### .create_object() function can lead to infinite recursive loops!
-.create_object <- function(objname)
-{
-    if (objname == "empty_extrasnplocs") {
-        obj <- data.frame(loc=integer(0), alleles=raw(0))
-        return(obj)
-    }
-    if (objname == "empty_ufsnplocs") {
-        obj <- data.frame(RefSNP_id=character(0),
-                          alleles_as_ambig=character(0),
-                          loc=integer(0),
-                          stringsAsFactors=FALSE)
-        return(obj)
-    }
-
-    # add more here...
-    stop("don't know how to create object '", objname, "'")
-}
-
-.get_ExtraSNPlocs_data <- function(x, objname, caching=TRUE)
-{
-    datacache <- x@.data_cache
-    not_cached <- !exists(objname, envir=datacache)
-    if (not_cached) {
-        if (objname %in% x@data_serialized_objnames) {
-            filename <- paste(objname, ".rda", sep="")
-            filepath <- file.path(x@data_dirpath, filename)
-            load(filepath, envir=datacache)
-        } else {
-            assign(objname, .create_object(objname), envir=datacache)
-        }
-    }
-    ans <- get(objname, envir=datacache)
-    if (not_cached && !caching)
-        rm(list=objname, envir=datacache)
-    ans
-}
-
 setMethod("snpcount", "ExtraSNPlocs",
     function(x)
     {
-        objname <- "ExtraSNPcount"
-        ans <- .get_ExtraSNPlocs_data(x, objname)
-        if (!is.integer(ans) || !identical(names(ans), seqlevels(x)))
-            stop("internal error: '", objname, "' data set is broken.\n",
-                 "       Please contact the maintainer of the ",
-                 x@data_pkgname, "\n       package.")
-        ans        
+        stop("NOT READY YET!")        
     }
 )
-
-### Load raw extra snplocs.
-.load_raw_extrasnplocs <- function(x, seqname, caching)
-{
-    objname <- paste(seqname, "_extrasnplocs", sep="")
-    ans <- .get_ExtraSNPlocs_data(x, objname, caching=caching)
-    empty_extrasnplocs <- .get_ExtraSNPlocs_data(x, "empty_extrasnplocs")
-    if (!identical(sapply(ans, class), sapply(empty_snplocs, class)))
-        stop("internal error: unexpected col names and/or col types\n",
-             "       for the '", objname, "' data set.\n",
-             "       Please contact the maintainer of the ",
-             x@data_pkgname, "\n       package.")
-    if (nrow(ans) != snpcount(x)[seqname])
-        stop("internal error: nb of rows in object '", objname, " ",
-             "doesn't match the\n       nb of SNPs reported by snpcount().\n",
-             "       Please contact the maintainer of the ",
-             x@data_pkgname, "\n       package.")
-    ans
-}
-
-.get_rsid_offsets <- function(x)
-{
-    offsets <- c(0L, cumsum(snpcount(x)))
-    offsets <- offsets[-length(offsets)]
-    names(offsets) <- names(snpcount(x))
-    offsets
-}
-
-### Load rs ids for a given sequence. Return them in an integer vector.
-.load_rsids <- function(x, seqname)
-{
-    all_rsids <- .get_ExtraSNPlocs_data(x, "all_rsids")
-    seq_pos <- match(seqname, names(snpcount(x)))
-    offset <- .get_rsid_offsets(x)[seq_pos]
-    idx <- seq_len(snpcount(x)[seq_pos]) + offset
-    all_rsids[idx]
-}
-
-### Get user-friendly extra snplocs.
-.get_ufextrasnplocs <- function(x, seqname, caching)
-{
-    rsids <- as.character(.load_rsids(x, seqname))
-    extrasnplocs <- .load_raw_extrasnplocs(x, seqname, caching)
-    alleles <- safeExplode(rawToChar(extrasnplocs$alleles))
-    data.frame(RefSNP_id=rsids,
-               alleles_as_ambig=alleles,
-               loc=extrasnplocs$loc,
-               stringsAsFactors=FALSE)
-}
-
-.ExtraSNPlocsAsGranges <- function(x, ufextrasnplocs, seqname)
-{
-    if (is(seqname, "Rle")) {
-        if (length(seqname) != nrow(ufextrasnplocs)
-         || !identical(levels(seqname), seqlevels(x)))
-            stop("when an Rle, 'seqname' must be a factor Rle ",
-                 "of length 'nrow(ufextrasnplocs)' and levels 'SEQNAMES'")
-        ans_seqnames <- seqname
-    } else {
-        if (!is.factor(seqname))
-            seqname <- factor(seqname, levels=seqlevels(x))
-        if (length(seqname) == 1L)
-            ans_seqnames <- Rle(seqname, nrow(ufextrasnplocs))
-        else if (length(seqname) == nrow(ufextrasnplocs))
-            ans_seqnames <- Rle(seqname)
-        else
-            stop("'length(seqname)' must be 1 or 'nrow(ufextrasnplocs)'")
-    }
-    if (nrow(ufextrasnplocs) == 0L)
-        ans_ranges <- IRanges()
-    else
-        ans_ranges <- IRanges(start=ufextrasnplocs$loc, width=1L)
-    ans_strand <- Rle(strand("+"), nrow(ufextrasnplocs))
-    ans <- GRanges(seqnames=ans_seqnames,
-                   ranges=ans_ranges,
-                   strand=ans_strand,
-                   RefSNP_id=ufextrasnplocs$RefSNP_id,
-                   alleles_as_ambig=ufextrasnplocs$alleles_as_ambig)
-    seqinfo(ans) <- seqinfo(x)
-    ans
-}
 
 ### Returns a data frame (when 'as.GRanges=FALSE') or a GRanges object
 ### (when 'as.GRanges=TRUE').
 setMethod("snplocs", "ExtraSNPlocs",
     function(x, seqname, as.GRanges=FALSE, caching=TRUE)
     {
-        if (!is.character(seqname) || any(is.na(seqname)))
-            stop("'seqname' must be a character vector with no NAs")
-        if (!all(seqname %in% seqlevels(x)))
-            stop("all 'seqname' elements must be in: ",
-                 paste(seqlevels(x), collapse=", "))
-        if (!isTRUEorFALSE(as.GRanges))
-            stop("'as.GRanges' must be TRUE or FALSE")
-        if (!isTRUEorFALSE(caching))
-            stop("'caching' must be TRUE or FALSE")
-        if (!as.GRanges) {
-            if (length(seqname) != 1L)
-                stop("'seqname' must be of length 1 when 'as.GRanges' is FALSE")
-            return(.get_ufextrasnplocs(x, seqname, caching))
-        }
-        if (length(seqname) == 0L) {
-            empty_ufextrasnplocs <- .get_ExtraSNPlocs_data(x, "empty_ufextrasnplocs")
-            ans <- .ExtraSNPlocsAsGranges(x, empty_ufextrasnplocs, character(0))
-            return(ans)
-        }
-        list_of_ufextrasnplocs <- lapply(seqname,
-                                    .get_ufextrasnplocs, x=x, caching=caching)
-        ufextrasnplocs <- do.call(rbind, list_of_ufextrasnplocs)
-        seqnames <- Rle(factor(seqname, levels=seqlevels(x)),
-                        unlist(lapply(list_of_ufextrasnplocs, nrow), use.names=FALSE))
-        .ExtraSNPlocsAsGranges(x, ufextrasnplocs, seqnames)
+        stop("NOT READY YET!")
     }
 )
 
@@ -281,117 +122,24 @@ setMethod("snplocs", "ExtraSNPlocs",
 ### snpid2loc(), snpid2alleles(), and snpid2grange()
 ###
 
-.normargSnpid <- function(snpid)
-{
-    if (!is.vector(snpid))
-        stop("'snpid' must be an integer or character vector")
-    if (S4Vectors:::anyMissing(snpid))
-        stop("'snpid' cannot contain NAs")
-    if (is.numeric(snpid)) {
-        if (!is.integer(snpid))
-            snpid <- as.integer(snpid)
-        return(snpid)
-    }
-    if (!is.character(snpid))
-        stop("'snpid' must be an integer or character vector")
-    prefixes <- unique(substr(snpid, 1L, 2L))
-    if ("rs" %in% prefixes) {
-        if (!setequal(prefixes, "rs"))
-            stop("'snpid' cannot mix ids that are prefixed with \"rs\" ",
-                 "with ids that are not")
-        ## Drop the "rs" prefix.
-        snpid <- substr(snpid, 3, nchar(snpid))
-    }
-    snpid <- suppressWarnings(as.integer(snpid))
-    if (S4Vectors:::anyMissing(snpid))
-        stop("cannot extract the digital part of some ids in 'snpid'")
-    snpid
-}
-
-### Returns a named integer vector where each (name, value) pair corresponds
-### to a supplied SNP id (typically an rs id). The name is the chromosome of
-### the SNP id and the value is the row index in the serialized extrasnplocs
-### data frame corresponding to the SNP id.
-.snpid2rowidx <- function(x, snpid)
-{
-    if (length(snpid) == 0L) {
-        idx <- integer(0)
-    } else {
-        all_rsids <- .get_ExtraSNPlocs_data(x, "all_rsids")
-        idx <- match(snpid, all_rsids)
-        bad_snpid_idx <- which(is.na(idx))
-        if (length(bad_snpid_idx) != 0L) {
-            bad_snpid <- snpid[bad_snpid_idx]
-            bad_snpid_in1string <- paste(bad_snpid, collapse=", ")
-            stop("SNP id(s) not found: ", bad_snpid_in1string)
-        }
-    }
-    seqidx <- findInterval(idx - 1L, cumsum(snpcount(x))) + 1L
-    rowidx <- idx - .get_rsid_offsets(x)[seqidx]
-    names(rowidx) <- names(snpcount(x))[seqidx]
-    rowidx
-}
-
 setMethod("snpid2loc", "ExtraSNPlocs",
     function(x, snpid, caching=TRUE)
     {
-        snpid <- .normargSnpid(snpid)
-        if (!isTRUEorFALSE(caching))
-            stop("'caching' must be TRUE or FALSE")
-        rowidx <- .snpid2rowidx(x, snpid)
-        if (length(rowidx) == 0L) {
-            ans <- integer(0)
-        } else {
-            rowidx_list <- split(unname(rowidx), names(rowidx))
-            loc_list <- lapply(names(rowidx_list),
-                function(seqname) {
-                    idx <- rowidx_list[[seqname]]
-                    .load_raw_extrasnplocs(x, seqname, caching)$loc[idx]
-                })
-            ans <- unsplit(loc_list, names(rowidx))
-        }
-        names(ans) <- names(rowidx)
-        ans
+        stop("NOT READY YET!")
     }
 )
 
 setMethod("snpid2alleles", "ExtraSNPlocs",
     function(x, snpid, caching=TRUE)
     {
-        snpid <- .normargSnpid(snpid)
-        if (!isTRUEorFALSE(caching))
-            stop("'caching' must be TRUE or FALSE")
-        rowidx <- .snpid2rowidx(x, snpid)
-        if (length(rowidx) == 0L) {
-            ans <- raw(0)
-        } else {
-            rowidx_list <- split(unname(rowidx), names(rowidx))
-            alleles_list <- lapply(names(rowidx_list),
-                function(seqname) {
-                    idx <- rowidx_list[[seqname]]
-                    .load_raw_extrasnplocs(x, seqname, caching)$alleles[idx]
-                })
-            ans <- unsplit(alleles_list, names(rowidx))
-        }
-        ans <- safeExplode(rawToChar(ans))
-        names(ans) <- names(rowidx)
-        ans
+        stop("NOT READY YET!")
     }
 )
 
 setMethod("snpid2grange", "ExtraSNPlocs",
     function(x, snpid, caching=TRUE)
     {
-        snpid <- .normargSnpid(snpid)
-        if (!isTRUEorFALSE(caching))
-            stop("'caching' must be TRUE or FALSE")
-        loc <- snpid2loc(x, snpid, caching=caching)
-        alleles <- snpid2alleles(x, snpid, caching=caching)
-        ufextrasnplocs <- data.frame(RefSNP_id=as.character(snpid),
-                                alleles_as_ambig=unname(alleles),
-                                loc=unname(loc),
-                                stringsAsFactors=FALSE)
-        .ExtraSNPlocsAsGranges(x, ufextrasnplocs, names(loc))
+        stop("NOT READY YET!")
     }
 )
 
