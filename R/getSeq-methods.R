a23b70c0cbde7d6c90b96fad2155b7119cebcab6
Herve Pages <hpages@fhcrc.org>
2014-10-31 01:49:05 +0000

    Remove support for RangedData in "getSeq" method for BSgenome objects (was defunct since Bioc 2.14).
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BSgenome@96258 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getSeq-methods.R
+++ R/getSeq-methods.R
@@ -86,18 +86,6 @@
 ### .toGRanges() is called.
 .toGRanges <- function(x, strand)
 {
-    if (is(x, "RangedData")) {
-        msg <- c("Starting with BioC 2.14, passing a RangedData object to ",
-                 "getSeq() is no more supported. Please use a GRanges object ",
-                 "instead. Note that you can obtain a GRanges object by ",
-                 "simply coercing the RangedData object with ",
-                 "'as(..., \"RangedData\")'. However, we strongly recommend ",
-                 "that you start migrating your code to operate on GRanges ",
-                 "objects instead of RangedData objects. Please ask on the ",
-                 "bioc-devel mailing list if you have questions or concerns ",
-                 "about this (http://bioconductor.org/help/mailing-list/).")
-        .Defunct(msg=msg)
-    }
     if (is(x, "RangesList") || is(x, "Ranges")) {
         if (is.null(names(x)))
             stop("when 'names' is a RangesList or Ranges object, ",
