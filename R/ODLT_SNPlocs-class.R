c583eea987185cd5cdd16ebc6a66c70913a27136
Hervé Pagès <hpages@fredhutch.org>
2019-12-13 11:25:06 -0800

    The SNP extractors now return the ref allele and alt allele(s)
    - Add new utility inferRefAndAltAlleles() for inferring the ref allele
      and alt allele(s) from the metadata column "alleles_as_ambig" of a
      GPos object and from the actual sequences of the reference genome.
    
    - Add new 'genome' argument to SNP extractors snpsBySeqname(),
      snpsByOverlaps(), and snpsById() for specifying the reference
      genome of the SNPs. When this argument is specified,
      inferRefAndAltAlleles() is called internally to **infer**
      the ref allele and alt allele(s) for each SNP.

--- R/ODLT_SNPlocs-class.R
+++ R/ODLT_SNPlocs-class.R
@@ -74,32 +74,95 @@ setMethod("snplocs", "ODLT_SNPlocs",
 
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### .as_naked_GRanges() and .as_GPos()
+### inferRefAndAltAlleles()
 ###
-
-.as_naked_GRanges <- function(df, seqinfo)
+### Infers the ref allele and alt allele(s) for each SNP in 'gpos'.
+### 'gpos' must be a GPos derivative containing SNPs. It must have a metadata
+### column "alleles_as_ambig" like obtained when using any of the SNP
+### extractors snpsBySeqname(), snpsByOverlaps(), or snpsById() on a SNPlocs
+### object.
+### For each SNP the ref allele is inferred from the nucleotide
+### found in reference genome 'genome' at the SNP position.
+### The alt alleles are inferred from metadata column "alleles_as_ambig" and
+### the ref allele. More precisely for each SNP the alt alleles are considered
+### to be the alleles in "alleles_as_ambig" minus the ref allele.
+### Return a DataFrame with one row per SNP in 'gpos' and with columns
+### "genome_compat" (logical), "ref_allele" (character), and "alt_alleles"
+### (CharacterList).
+inferRefAndAltAlleles <- function(gpos, genome)
 {
-    ans_seqnames <- df[ , "seqnames"]
-    ans_ranges <- IRanges(df[ , "pos"], width=1L)
-    ans_strand <- Rle(strand("*"), nrow(df))
-    GRanges(ans_seqnames, ans_ranges, ans_strand, seqinfo=seqinfo)
+    ## Check 'gpos'.
+    if(!is(gpos, "GPos"))
+        stop(wmsg("'gpos' must be a GPos derivative"))
+
+    ## Check metadata column "alleles_as_ambig".
+    alleles_as_ambig <- mcols(gpos)$alleles_as_ambig
+    if (!is.character(alleles_as_ambig))
+        stop(wmsg("'gpos' must have metadata column \"alleles_as_ambig\" ",
+                  "and it must be a character vector"))
+    alleles <- IUPAC_CODE_MAP[alleles_as_ambig]
+    if (anyNA(alleles))
+        stop(wmsg("invalid metadata column \"alleles_as_ambig\""))
+    alleles <- DNAStringSet(alleles)
+
+    ## Check 'genome'.
+    genome <- getBSgenome(genome)
+
+    ## Compute 'ref_allele'.
+    ## getSeq() will complain if 'seqinfo(gpos)' and 'seqinfo(genome)' use
+    ## different genome names (e.g. GRCh38.p2 and GRCh38) so we get rid of
+    ## the genome information in 'gpos'.
+    genome(gpos) <- NA_character_
+    ref_allele <- getSeq(BSgenome.Hsapiens.NCBI.GRCh38, gpos)
+
+    ## Compute 'genome_compat'.
+    ## Note that a small percentage of SNPs in dbSNP have alleles that
+    ## are inconsistent with the reference genome (don't ask me why).
+    af <- alphabetFrequency(alleles, baseOnly=TRUE)[ , 1:4, drop=FALSE]
+    ref_af <- alphabetFrequency(ref_allele, baseOnly=TRUE)[ , 1:4, drop=FALSE]
+    genome_compat <- matrixStats::rowAlls(af >= ref_af)
+
+    ## Compute 'alt_alleles'.
+    gpos_len <- length(gpos)
+    idx <- relist(as.logical(t((af - ref_af) >= 1L)),
+                  PartitioningByEnd(seq_len(gpos_len) * 4L))
+    alt_alleles <- rep.int(CharacterList(colnames(af)), gpos_len)[idx]
+
+    DataFrame(genome_compat=genome_compat,
+              ref_allele=as.character(ref_allele),
+              alt_alleles=alt_alleles)
 }
 
-.as_GPos <- function(df, seqinfo, drop.rs.prefix=FALSE)
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### .as_naked_GPos() and .as_GPos()
+###
+
+.as_naked_GPos <- function(df, seqinfo)
 {
-    gr <- .as_naked_GRanges(df, seqinfo)
+    ans_seqnames <- df[ , "seqnames"]
+    ans_pos <- IPos(df[ , "pos"])
+    ans_strand <- Rle(strand("*"), nrow(df))
+    GPos(ans_seqnames, ans_pos, ans_strand, seqinfo=seqinfo)
+}
+
+.as_GPos <- function(df, seqinfo, drop.rs.prefix=FALSE, genome=NULL)
+{
+    gpos <- .as_naked_GPos(df, seqinfo)
     alleles_as_ambig <- decode_bytes_as_letters(df[ , "alleles"])
     rowids <- df$rowids
     if (is.null(rowids)) {
         ans_mcols <- DataFrame(alleles_as_ambig=alleles_as_ambig)
     } else {
-        if (!drop.rs.prefix && length(gr) != 0L)
+        if (!drop.rs.prefix && length(gpos) != 0L)
             rowids <- paste0("rs", rowids)
         ans_mcols <- DataFrame(RefSNP_id=rowids,
                                alleles_as_ambig=alleles_as_ambig)
     }
-    mcols(gr) <- ans_mcols
-    as(gr, "GPos")
+    mcols(gpos) <- ans_mcols
+    if (!is.null(genome))
+        mcols(gpos) <- cbind(ans_mcols, inferRefAndAltAlleles(gpos, genome))
+    gpos
 }
 
 
@@ -107,7 +170,8 @@ setMethod("snplocs", "ODLT_SNPlocs",
 ### SNP extractors: snpsBySeqname(), snpsByOverlaps(), snpsById()
 ###
 
-.snpsBySeqname_ODLT_SNPlocs <- function(x, seqnames, drop.rs.prefix=FALSE)
+.snpsBySeqname_ODLT_SNPlocs <- function(x, seqnames, drop.rs.prefix=FALSE,
+                                                     genome=NULL)
 {
     if (!isTRUEorFALSE(drop.rs.prefix))
         stop(wmsg("'drop.rs.prefix' must be TRUE or FALSE"))
@@ -116,12 +180,13 @@ setMethod("snplocs", "ODLT_SNPlocs",
                                                  with.rowids=TRUE)
     x_spatial_index <- spatialIndex(x@snp_table)
     x_seqinfo <- seqinfo(x_spatial_index)
-    .as_GPos(df, x_seqinfo, drop.rs.prefix=drop.rs.prefix)
+    .as_GPos(df, x_seqinfo, drop.rs.prefix=drop.rs.prefix, genome=genome)
 }
 
 setMethod("snpsBySeqname", "ODLT_SNPlocs", .snpsBySeqname_ODLT_SNPlocs)
 
-.snpsByOverlaps_ODLT_SNPlocs <- function(x, ranges, drop.rs.prefix=FALSE, ...)
+.snpsByOverlaps_ODLT_SNPlocs <- function(x, ranges, drop.rs.prefix=FALSE, ...,
+                                                    genome=NULL)
 {
     ranges <- normarg_ranges(ranges)
     if (!isTRUEorFALSE(drop.rs.prefix))
@@ -140,16 +205,17 @@ setMethod("snpsBySeqname", "ODLT_SNPlocs", .snpsBySeqname_ODLT_SNPlocs)
                                                   with.rowids=TRUE)
     x_spatial_index <- spatialIndex(x@snp_table)
     x_seqinfo <- seqinfo(x_spatial_index)
-    gr0 <- .as_naked_GRanges(df, x_seqinfo)
-    idx <- which(overlapsAny(gr0, ranges, ...))
+    gpos0 <- .as_naked_GPos(df, x_seqinfo)
+    idx <- which(overlapsAny(gpos0, ranges, ...))
     df <- df[idx, ]
-    .as_GPos(df, x_seqinfo, drop.rs.prefix=drop.rs.prefix)
+    .as_GPos(df, x_seqinfo, drop.rs.prefix=drop.rs.prefix, genome=genome)
 }
 
 setMethod("snpsByOverlaps", "ODLT_SNPlocs", .snpsByOverlaps_ODLT_SNPlocs)
 
 .snpsById_ODLT_SNPlocs <- function(x, ids,
-                                   ifnotfound=c("error", "warning", "drop"))
+                                   ifnotfound=c("error", "warning", "drop"),
+                                   genome=NULL)
 {
     user_rowids <- ids2rowids(ids)
     ifnotfound <- match.arg(ifnotfound)
@@ -161,7 +227,7 @@ setMethod("snpsByOverlaps", "ODLT_SNPlocs", .snpsByOverlaps_ODLT_SNPlocs)
     df$rowids <- rowidx[[2L]]
     x_spatial_index <- spatialIndex(x@snp_table)
     x_seqinfo <- seqinfo(x_spatial_index)
-    .as_GPos(df, x_seqinfo, drop.rs.prefix=TRUE)
+    .as_GPos(df, x_seqinfo, drop.rs.prefix=TRUE, genome=genome)
 }
 
 setMethod("snpsById", "ODLT_SNPlocs", .snpsById_ODLT_SNPlocs)
