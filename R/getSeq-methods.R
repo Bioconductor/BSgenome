b2e4e593d3cbd1989ef1e93dfbcd7cfd6a29bcbc
LiNk-NY <mramos09@gmail.com>
2021-04-26 18:45:01 -0400

    move getSeq,XStringSet-method out

--- R/getSeq-methods.R
+++ R/getSeq-methods.R
@@ -95,7 +95,7 @@
         else
             ans <- .newGRangesFromNamedRanges(x, strand)
         return(ans)
-    } 
+    }
     stop("invalid 'names'")
 }
 
@@ -352,53 +352,3 @@ setMethod("getSeq", "BSgenome",
         ans
     }
 )
-
-setMethod("getSeq", "XStringSet", 
-    function(x, names) 
-    {
-        stopifnot(is.character(names) || is(names, "GRanges") ||
-                  is(names, "GRangesList"),
-                  !is.null(names(x)))
-        if (is.character(names)) {
-            found <- names %in% names(x)
-            regexNames <- unlist(lapply(names[!found], grep, names(x),
-                                        value=TRUE))
-            names <- c(names[found], regexNames)
-            return(x[names])
-        } else if (is(names, "GRangesList")) {
-            gr <- unlist(names, use.names=FALSE)
-        } else {
-            gr <- names
-        }
-        ignoringStrand <- any(strand(gr) != "*") &&
-            !hasMethod(reverseComplement, class(x))
-        if (ignoringStrand) {
-            warning("some strand(x) != '*' but ",
-                    "strand has no meaning for ", class(x))
-        }
-        rl <- as(gr, "IntegerRangesList")
-        ans <- unsplit(extractAt(x[names(rl)], unname(rl)), seqnames(gr))
-        if (!ignoringStrand) {
-            minus <- strand(gr) == "-"
-            ans[minus] <- reverseComplement(ans[minus])
-        }
-        if (is(names, "GRangesList")) {
-            ans <- relist(ans, names)
-        }
-        ans
-    }
-)
-
-setMethod("[", c("XStringSet", "GenomicRanges"),
-          function (x, i, j, ..., drop = TRUE) {
-              if (!missing(j)) 
-                  stop("invalid subsetting")
-              getSeq(x, i, ...)
-          })
-
-setMethod("[", c("XStringSet", "GRangesList"),
-          function (x, i, j, ..., drop = TRUE) {
-              if (!missing(j)) 
-                  stop("invalid subsetting")
-              unstrsplit(getSeq(x, i, ...))
-          })
